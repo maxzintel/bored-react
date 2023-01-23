98400766b99d99b28083608a6d628a73b368bfba
maxzintel <maxzintel@gmail.com>
2023-01-22 19:52:56 -0500

    More WIP

--- bored/App.js
+++ bored/App.js
@@ -1,12 +1,28 @@
 import React, { useRef, useEffect } from 'react';
-import { Button, StyleSheet, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import LottieView from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const TitleView = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Welcome to Bored</Text>
       <Text style={styles.subtitle}>The app designed to make you more Bored.</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PreQuestionView')}>
+        <Text style={styles.buttonText}>Let's Go!</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
+const PreQuestionView = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      
     </View>
   )
 };
@@ -28,7 +44,19 @@ const styles = {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+    alignSelf: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
 };
 
 export default TitleView;
